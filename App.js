1f7698b57a3744b5f8368c61c076a5279fcc1456
Felipe Dantas <felipedantas1995@gmail.com>
2022-03-07 08:45:50 -0300

    Navigation duas telas Home e Canal

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ function TelaHome({navigation}){
         <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
             <Text>Tela Home</Text>
             <Text>First Screen</Text>
+            <Button title="Ir para Canal" onPress={()=>navigation.navigate('Canal')}/>
         </View>
     )
 }
@@ -19,6 +20,8 @@ function TelaCanal({navigation}){
       <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
           <Text>Tela Canal</Text>
           <Text>Second Screen</Text>
+          <Button title="Ir para Home" onPress={()=>navigation.navigate('Home')}/>
+          <Button title="Voltar" onPress={()=>navigation.goBack('Home')}/>
       </View>
   )
 }
@@ -28,7 +31,7 @@ export default function () {
   
 
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="Tela Home">
      <Pilha.Navigator>
         <Pilha.Screen name="Home" component={TelaHome} options={{title:'Tela Inicial'}}/>
         <Pilha.Screen name="Canal" component={TelaCanal} options={{title:'Tela Secundaria'}}/>
